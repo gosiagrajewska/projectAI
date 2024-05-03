074509a6077847cf8c51d2de78aa4e15fed17991
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-05-03 11:39:06 +0200

    added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generatePoem(event) {
+  event.preventDefault();
+  alert("Generating Poem");
+  /*let poemBox = document.querySelector("#poem");
+  poemBox.innerHTML = "The poem will be displayed here";*/
 
+  new Typewriter("#poem", {
+    strings: "The poem will be displayed here",
+    autoStart: true,
+    cursor: "",
+    delay: 100,
+  });
+}
+
+let poemFormElement = document.querySelector("#generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
