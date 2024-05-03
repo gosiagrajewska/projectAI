8685aa08f7412b1934ea8451fa3f7c904ae29e93
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-05-03 15:09:34 +0200

    UX optimized

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ function generatePoem(event) {
 
   let apiKey = "b0aa3bbe4a50o046t63f82874a31fac6";
   let context =
-    "You are a poet who writes short and funny poems which rhyme. The answer you provide is in HTML format. The answer is maximum 4 lines of poetry. In the answer after the poem you show your fictional signature. Show the poem only, without its title. The answer is in font Briem Handin. In the answer the lines brake always after the rhymes. ";
+    "You are a poet who writes short and funny poems which rhyme. The answer is in HTML format. The answer is maximum 4 lines of poetry. In the answer after the poem you show your fictional signature in bold font. Show the poem only, without its title. In the answer the lines brake always after the rhymes. ";
   let input = document.querySelector("#generator-input");
 
-  let prompt = `Generate a poem about ${input.value}  `;
+  let prompt = `Generate a poem about ${input.value} and give answer in HTML `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
 
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${input.value}</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 function displayPoem(response) {
@@ -16,7 +19,7 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 100,
+    delay: 50,
   });
 }
 
